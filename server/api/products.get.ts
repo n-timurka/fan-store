a58af691f39d3fc384adf4b266c9afe97ce570c7
Timurka Nikolay <timurka.nick@gmail.com>
2024-10-03 17:07:24 +0200

    category filter fix

--- server/api/products.get.ts
+++ server/api/products.get.ts
@@ -4,6 +4,6 @@ export default defineEventHandler(async (event) => {
   const { category } = getQuery(event);
 
   return category
-    ? products.filter((product) => product.category === category)
+    ? products.filter((product) => category.includes(product.category))
     : products;
 });
